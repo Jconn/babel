de589144212deeb21f0f0c92313c4a03679a8581
jconnolly <johnconn@umich.edu>
2018-11-04 17:51:21 -0500

    initial oled add (nkolban stuff and the u8g2 library)

--- component.mk
+++ component.mk
@@ -4,6 +4,6 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 #
 
-COMPONENT_SRCDIRS := . babel_gen
-COMPONENT_ADD_INCLUDEDIRS := . babel_gen
+COMPONENT_SRCDIRS := . babel_gen u8g2/csrc nkolban
+COMPONENT_ADD_INCLUDEDIRS := . babel_gen u8g2/csrc nkolban
 
--- eeprom_config.c
+++ eeprom_config.c
@@ -28,6 +28,8 @@
 #include "gatts_demo.h"
 
 
+#include "u8g2.h"
+#include "u8g2_esp32_hal.h"
 #define EEPROM_I2C_ADDR (0x50)
 
 //
@@ -336,10 +338,57 @@ static void i2c_example_master_init(void)
                        0);
 }
 
+// CLK - GPIO14
+#define PIN_CLK 14
+
+// MOSI - GPIO 13
+#define PIN_MOSI 13
+
+// RESET - GPIO 26
+#define PIN_RESET 26
+
+// DC - GPIO 27
+#define PIN_DC 27
+
+// CS - GPIO 15
+#define PIN_CS 15
+
+void set_display(u8g2_t *u8g2)
+{
+    //setup the hal state for the display callback(this is kolban specific)
+    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+    u8g2_esp32_hal.clk   = PIN_CLK;
+    u8g2_esp32_hal.mosi  = PIN_MOSI;
+    u8g2_esp32_hal.cs    = PIN_CS;
+    u8g2_esp32_hal.dc    = PIN_DC;
+    u8g2_esp32_hal.reset = PIN_RESET;
+    u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+    u8g2_Setup_ssd1306_128x64_noname_f(
+            u8g2,
+            U8G2_R0,
+            u8g2_esp32_spi_byte_cb,
+            u8g2_esp32_gpio_and_delay_cb);  // init u8g2 structure
+
+    u8g2_InitDisplay(u8g2); // send init sequence to the display, display is in sleep mode after this,
+    u8g2_SetPowerSave(u8g2, 0); // wake up display
+
+}
 static void eeprom_poll(void* arg)
 {
+
+    u8g2_t u8g2; // a structure which will contain all the data for one display
     i2c_example_master_init();
     activate_adc();
+    /* setup display */
+
+    set_display(&u8g2);
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_DrawBox(&u8g2, 10,20, 20, 30);
+    u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
+    u8g2_DrawStr(&u8g2, 0,15,"Hello World!");
+    u8g2_SendBuffer(&u8g2);
+
     while (1) {
 
         int32_t current_profile = get_eeprom_profile();
