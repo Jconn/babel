ec56170d75d39f0ccb6d06a864398e9f0541557c
jconnolly <johnconn@umich.edu>
2018-11-14 22:26:36 -0500

    missed a change

--- eeprom_config.c
+++ eeprom_config.c
@@ -50,17 +50,6 @@
 */
 
 
-#define I2C_EXAMPLE_MASTER_SCL_IO          19               /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO          18               /*!< gpio number for I2C master data  */
-#define EEPROM_PORT             I2C_NUM_1        /*!< I2C port number for master dev */
-#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
-
-
-#define PIN_SDA I2C_EXAMPLE_MASTER_SDA_IO
-#define PIN_SCL I2C_EXAMPLE_MASTER_SCL_IO
-
 //
 //from the datasheet  - pages are 16 bytes long
 //                      the first 8 bytes set up the page write
